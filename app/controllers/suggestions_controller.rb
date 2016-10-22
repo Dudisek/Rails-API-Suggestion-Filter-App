f5d8e4deb846f8d4e07fac6a8d5c9e7b6c6ba00c
Dudisek <peta.duda@gmail.com>
2016-10-22 08:27:14 +0100

    edit format category

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,8 +2,22 @@ class SuggestionsController < ApplicationController
 
   def index
     products = Product.filter(params)
-    products = products.as_json(except: [:created_at, :updated_at])
-    render json: {suggestions: products}
+    render json: {suggestions: edit_format(products)}
   end
 
+  private
+
+  def edit_format(products)
+    # Remove created_at and updated_at from result
+    products = products.as_json(except: [:created_at, :updated_at])
+    # Edit and translate category
+    products.each do |p|
+      p['category'] = {
+      'id' => p['category'], 
+      'trans' => {
+        'en' => (I18n.t "cat.#{p['category']}"), 
+        'es' => (I18n.t "cat.#{p['category']}", locale: :es)
+      }}
+    end
+  end
 end
